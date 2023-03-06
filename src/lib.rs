ce1d714f9e1b9ddfb998d691be9c2fc44f4e128d
Jérôme Boillot <me@jerome-boillot.com>
2023-03-06 13:47:39 +0100

    Add WSL and SSH support

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,10 @@
 extern crate napi_derive;
 
 use std::borrow::Cow;
+use std::env;
+use std::io::Write;
+use std::process::{Command, Stdio};
+use duct::cmd;
 
 use napi::{bindgen_prelude::*, JsBuffer};
 
@@ -25,18 +29,37 @@ impl Clipboard {
     })
   }
 
+  /// Copy text to the clipboard. Has special handling for WSL and SSH sessions, otherwise
+  /// falls back to the cross-platform `clipboard` crate
   #[napi]
   pub fn set_text(&mut self, text: String) -> Result<()> {
-    self
-      .inner
-      .set_text(text)
-      .map_err(clipboard_error_to_js_error)?;
+    if wsl::is_wsl() {
+      set_wsl_clipboard(text)?;
+    } else if env::var("SSH_CLIENT").is_ok() {
+      // we're in an SSH session, so set the clipboard using OSC 52 escape sequence
+      set_clipboard_osc_52(text);
+    } else {
+      // we're probably running on a host/primary OS, so use the default clipboard
+      self
+        .inner
+        .set_text(text)
+        .map_err(clipboard_error_to_js_error)?;
+    }
+
     Ok(())
   }
 
   #[napi]
   pub fn get_text(&mut self) -> Result<String> {
-    self.inner.get_text().map_err(clipboard_error_to_js_error)
+    if wsl::is_wsl() {
+      let stdout = cmd!("powershell.exe", "get-clipboard").read()?;
+      Ok(stdout.trim().to_string())
+    } else if env::var("SSH_CLIENT").is_ok() {
+      Err(Error::new(Status::GenericFailure, "SSH clipboard not supported"))
+    } else {
+      // we're probably running on a host/primary OS, so use the default clipboard
+      self.inner.get_text().map_err(clipboard_error_to_js_error)
+    }
   }
 
   #[napi]
@@ -71,4 +94,22 @@ impl Clipboard {
       })
       .map_err(clipboard_error_to_js_error)
   }
+}
+
+/// Set the clipboard contents using OSC 52 (picked up by most terminals)
+fn set_clipboard_osc_52(text: String) {
+  print!("\x1B]52;c;{}\x07", base64::encode(text));
+}
+
+/// Set the Windows clipboard using clip.exe in WSL
+fn set_wsl_clipboard(s: String) -> Result<()> {
+  let mut clipboard = Command::new("clip.exe").stdin(Stdio::piped()).spawn()?;
+  let mut clipboard_stdin = clipboard
+    .stdin
+    .take()
+    .ok_or_else(|| Error::new(Status::GenericFailure, "Could not get stdin handle for clip.exe"))?;
+
+  clipboard_stdin.write_all(s.as_bytes())?;
+
+  Ok(())
 }
